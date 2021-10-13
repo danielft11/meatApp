c4ed76627354eae1338daf5fde2963c418ef1e8b
danielft11 <danielft11@hotmail.com>
2021-10-12 22:37:27 -0300

    Commit inicial até sessão 5: aplicação Meat

---
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
+import { Observable } from 'rxjs';
+import { Review } from './review.model';
+
+@Component({
+  selector: 'mt-reviews',
+  templateUrl: './reviews.component.html'
+})
+export class ReviewsComponent implements OnInit {
+
+  reviews: Observable<Review[]>
+
+  constructor(private restaurantsService: RestaurantsService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    //não tem o subscribe pois no HTML do componente está utilizando o pipe Async.
+    this.reviews = this.restaurantsService
+      .reviewsOfRestaurant(this.route.parent.snapshot.params['id']) 
+  }
+
+}
---
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from './cart-item.model';
+import { ShoppingCartService } from './shopping-cart-service';
+
+@Component({
+  selector: 'mt-shopping-cart',
+  templateUrl: './shopping-cart.component.html'
+})
+export class ShoppingCartComponent implements OnInit {
+
+  constructor(private shoppingCartService: ShoppingCartService) { }
+
+  ngOnInit() {}
+
+  items(): CartItem[] {
+    return this.shoppingCartService.items
+  }
+
+  addItem(item: any) {
+    this.shoppingCartService.addItem(item)
+  }
+
+  removeItem(item: any) {
+    this.shoppingCartService.removeItem(item)
+  }
+
+  clear() {
+    this.shoppingCartService.clear()
+  }
+
+  total(): number {
+    return this.shoppingCartService.total()
+  }
+
+}
