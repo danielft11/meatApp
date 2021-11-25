8588bf6510223675b89e4bcbde4727fea617e277
danielft11 <danielft11@hotmail.com>
2021-11-24 23:23:59 -0300

    Validações em Reactive Forms

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,7 +4,7 @@ import { ShoppingCartService } from 'app/restaurant-detail/shopping-cart/shoppin
 import { RadioOption } from 'app/shared/radio/radio-option.model';
 import { Order, OrderItem } from './order.model';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'mt-order',
@@ -21,20 +21,25 @@ export class OrderComponent implements OnInit {
     { label: 'Cartão Refeição', value: 'REF' },
   ]
 
+  emailPattern: RegExp = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  
+  numberPattern: RegExp = /^[0-9]*$/
+
   constructor(private shoppingCartService: ShoppingCartService,
     private router: Router,
     private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.orderForm = this.formBuilder.group({
-      name: this.formBuilder.control(''),
-      email: this.formBuilder.control(''),
-      emailConfirmation: this.formBuilder.control(''),
-      address: this.formBuilder.control(''),
-      number: this.formBuilder.control(''),
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(''),
-      paymentOptions: this.formBuilder.control(''),
-    })
+      paymentOptions: this.formBuilder.control('', [Validators.required]),
+    }, 
+    {validator: OrderComponent.equalsTo})
   }
 
   cartItems(): CartItem[] {
@@ -67,6 +72,19 @@ export class OrderComponent implements OnInit {
         this.shoppingCartService.clear()
       })
 
+  }
+
+  static equalsTo(group: AbstractControl) : {[key: string]: boolean} {
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+
+    if (!email || !emailConfirmation){
+      return undefined
+    }
+
+    if (email.value !== emailConfirmation.value){
+      return {emailsNotMath: true}
+    }
   }
 
 }
